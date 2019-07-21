85ee03aef5fc6b85980b058299252351805e7c99
HY <2494033822@qq.com>
2019-07-21 17:38:07 +0700

    状态栏透明

--- picker/src/main/java/com/hy/picker/ActivityUtils.kt
+++ picker/src/main/java/com/hy/picker/ActivityUtils.kt
@@ -356,6 +356,16 @@ fun setViewClick(view: View?, listener: (View) -> Unit, isShow: Boolean ) {
 fun Activity.setStatusTransparent() {
     val window = window
     window.attributes.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        //兼容5.0及以上支持全透明
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+    }
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         try {
             @SuppressLint("PrivateApi")
@@ -366,8 +376,8 @@ fun Activity.setStatusTransparent() {
             field.isAccessible = false
         } catch (ignore: Exception) {
         }
-
     }
+
 }
 
 fun Activity.setStatusTransparentLight() {
